fe3a1784519db98c52261730473ff82708808c7e
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-01-31 14:32:46 -0500

    feat: add layout

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,21 @@
+import Navbar from '@components/Navbar';
+import Head from 'next/head';
+
+function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>Portfolio</title>
+        <meta name="description" content="Portfolio by gustavoylc" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="mx-52">
+        <Navbar />
+      </header>
+      <main className="mx-52">{children}</main>
+    </>
+  );
+}
+
+export default Layout;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css';
+import { Dosis } from '@next/font/google';
+import Layout from './Layout';
+
+const dosis = Dosis({
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '700', '800'],
+  variable: '--font-dosis',
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${dosis.variable} font-sans`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  );
 }
