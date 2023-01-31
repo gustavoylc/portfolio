a44cd22e891da177448e85ce524eccee52c0b923
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-01-31 14:51:57 -0500

    feat: change hand icon

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -5,7 +5,7 @@ function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Portfolio</title>
+        <title>Portfolio | gustavoylc</title>
         <meta name="description" content="Portfolio by gustavoylc" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
