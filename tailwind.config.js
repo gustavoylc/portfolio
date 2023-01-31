545d72b1b3b7186a4a458cb24b64b70833c797f3
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-01-31 14:33:41 -0500

    feat: add tailwind headers sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   content: [
@@ -17,5 +18,16 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        h1: { fontSize: theme('fontSize.5xl') },
+        h2: { fontSize: theme('fontSize.4xl') },
+        h3: { fontSize: theme('fontSize.3xl') },
+        h4: { fontSize: theme('fontSize.2xl') },
+        h5: { fontSize: theme('fontSize.xl') },
+        h6: { fontSize: theme('fontSize.lg') },
+      });
+    }),
+  ],
 };
