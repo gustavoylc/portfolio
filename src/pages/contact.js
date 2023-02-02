7cab75b510974542221d5b40460dcc423b7c5160
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-02-02 15:57:39 -0500

    feat: test contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,39 @@
+import { motion } from 'framer-motion';
+
+const icon = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: 'rgba(255, 255, 255, 0)',
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: 'rgba(255, 255, 255, 1)',
+  },
+};
+
 function contact() {
-  return <div>contact</div>;
+  return (
+    <div className="container">
+      <motion.svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        className="w-[20%] overflow-visible stroke-white stroke-1"
+      >
+        <motion.path
+          d="M0 100V0l50 50 50-50v100L75 75l-25 25-25-25z"
+          variants={icon}
+          initial="hidden"
+          animate="visible"
+          transition={{
+            default: { duration: 2, ease: 'easeInOut' },
+            fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+          }}
+        />
+      </motion.svg>
+    </div>
+  );
 }
 
 export default contact;
