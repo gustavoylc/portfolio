085cc5b4e345240879bad249ba2417ce01d2aed4
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-02-12 00:39:39 -0500

    feat: add projects layout

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,25 @@
+import { motion } from 'framer-motion';
+
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={{
+        hidden: { opacity: 0 },
+        visible: {
+          opacity: 1,
+          transition: {
+            duration: 0.5,
+            ease: 'easeIn',
+          },
+        },
+      }}
+      className="h-screen pt-28"
+    >
+      <h1 className="text-[#3da6e5]">Projects</h1>
+    </motion.div>
+  );
 }
 
 export default Projects;
