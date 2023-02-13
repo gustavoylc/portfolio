e96a42bcf6c6ebeebef619f2d4e1424f318d590d
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-02-13 12:01:12 -0500

    style: change styles

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -16,7 +16,12 @@ function Layout({ children }) {
       <main
         className="px-5 max-w-5xl mx-auto md:px-20 pt-24 pb-5 
                    md:py-0 md:short:pt-24 md:tall:py-0
-                   lg:short:pt-24 lg:tall:py-0"
+                   lg:short:pt-24 lg:tall:py-0                   
+                  grid md:h-screen md:items-center 
+                  md:short:h-full md:short:items-start 
+                  md:tall:h-screen md:tall:items-center
+                  lg:short:h-full lg:short:items-start
+                  lg:tall:h-screen lg:tall:items-center"
       >
         {children}
       </main>
