2833ecf444333cf86bcba5619daa6874bc0873a0
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-02-13 12:01:29 -0500

    style: add animation

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,25 @@
 import ContactMe from '@components/ContactMe';
+import { motion } from 'framer-motion';
 
 function contact() {
   return (
-    <div className="h-screen flex flex-col place-content-center">
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={{
+        hidden: { opacity: 0 },
+        visible: {
+          opacity: 1,
+          transition: {
+            duration: 0.5,
+            ease: 'easeIn',
+          },
+        },
+      }}
+    >
       <h1 className="text-[#3da6e5]">Get In Touch</h1>
       <ContactMe />
-    </div>
+    </motion.div>
   );
 }
 
