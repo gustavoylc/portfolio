8d580b1e0e58d54ace11b73524d8db4c4c8942bb
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-02-22 13:58:54 -0500

    feat: add skills

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,4 +1,25 @@
 import { motion } from 'framer-motion';
+import {
+  HTMLIcon,
+  CSSIcon,
+  JavascriptIcon,
+  TypescriptIcon,
+  ReactIcon,
+  ReduxIcon,
+  SassIcon,
+  TailwindCSSIcon,
+  MaterialUIIcon,
+  NextJSIcon,
+  NodeJSIcon,
+  ExpressIcon,
+  MongoDBIcon,
+  MySQLIcon,
+  PostgreSQLIcon,
+  DockerIcon,
+  SequelizeIcon,
+  GitIcon,
+} from '@components/Icons';
+import Skill from '@components/Skill';
 
 function Skills() {
   return (
@@ -15,16 +36,63 @@ function Skills() {
           },
         },
       }}
-      className="h-screen flex flex-col place-content-center "
     >
       <h1 className="text-[#3da6e5]">Skills</h1>
-      <article className="grid items-center grid-cols-[2fr_1fr] gap-16">
-        <div>
-          <h5>
-            HTML CSS Javascript Typescript React Redux Sass Tailwindcss MUI
-            Nodejs Nextjs Express MongoDB Mysql Postgres Docker
-          </h5>
-        </div>
+      <article className="grid grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))] gap-x-5 gap-y-10 justify-items-center">
+        <Skill name="HTML">
+          <HTMLIcon />
+        </Skill>
+        <Skill name="CSS">
+          <CSSIcon />
+        </Skill>
+        <Skill name="Javascript">
+          <JavascriptIcon />
+        </Skill>
+        <Skill name="Typescript">
+          <TypescriptIcon />
+        </Skill>
+        <Skill name="React">
+          <ReactIcon />
+        </Skill>
+        <Skill name="Redux">
+          <ReduxIcon />
+        </Skill>
+        <Skill name="SASS">
+          <SassIcon />
+        </Skill>
+        <Skill name="TailwindCSS">
+          <TailwindCSSIcon />
+        </Skill>
+        <Skill name="Material UI">
+          <MaterialUIIcon />
+        </Skill>
+        <Skill name="NextJS">
+          <NextJSIcon />
+        </Skill>
+        <Skill name="NodeJS">
+          <NodeJSIcon />
+        </Skill>
+        <Skill name="Express">
+          <ExpressIcon />
+        </Skill>
+        <Skill name="Sequelize">
+          <SequelizeIcon />
+        </Skill>
+        <Skill name="MongoDB">
+          <MongoDBIcon />
+        </Skill>
+        <Skill name="MySQL">
+          <MySQLIcon />
+        </Skill>
+        <Skill name="PostgreSQL">
+          <PostgreSQLIcon />
+        </Skill>
+        <Skill name="Docker">
+          <DockerIcon />
+        </Skill>
+        <Skill name="Git">
+          <GitIcon />
+        </Skill>
       </article>
     </motion.div>
   );
