b7a6a711281a7442cd8d4022b2f3294b843ad85e
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-03-11 18:01:20 -0500

    feat: effect while tap

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,15 @@
+import { motion } from 'framer-motion';
+
 function Button({ disabled = false, type = 'button', children }) {
   return (
-    <button
+    <motion.button
+      whileTap={{ scale: 0.9 }}
       className=" border-gray-500 border px-3 p-1 rounded-lg hover:text-white hover:bg-gray-900 hover:border-white flex items-center content-center"
       type={type}
       disabled={disabled}
     >
       {children}
-    </button>
+    </motion.button>
   );
 }
 
