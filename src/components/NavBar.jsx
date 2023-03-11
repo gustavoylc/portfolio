10eab113571158c7e5d3150b0a8d700f27e90345
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-03-11 18:02:44 -0500

    feat: add mobile menu to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Sling as Hamburger } from 'hamburger-react';
+import MobileMenu from '@components/MobileMenu';
 import Logo from '../../public/logo.svg';
 import NavLink from './NavLink';
 
@@ -34,17 +35,19 @@ function NavBar() {
     setOpen((prev) => !prev);
   };
   return (
-    <nav className="fixed top-0 left-0 right-0 flex justify-between max-w-5xl items-center mx-auto p-2 py-4 md:px-20 backdrop-blur z-10">
+    <nav className="fixed top-0 left-0 right-0 max-w-5xl flex justify-between  mx-auto p-2 py-4 md:px-20 backdrop-blur z-10">
       <motion.div
         initial={{ x: -400, opacity: 0, scale: 0.8 }}
         animate={{ x: 0, opacity: 1, scale: 1 }}
         transition={{ duration: 1.2 }}
         className="px-2 md:px-0"
+        onClick={isOpen ? handleToggle : null}
       >
         <Link href="/">
           <Image src={Logo} alt="Gustavo's Logo" width={40} />
         </Link>
       </motion.div>
+
       <motion.ul
         className="md:flex md:gap-5 md:items-center hidden"
         initial={{ x: 400, opacity: 0, scale: 0.8 }}
@@ -61,6 +64,7 @@ function NavBar() {
           </li>
         ))}
       </motion.ul>
+
       <motion.div
         className="md:hidden"
         initial={{ x: 400, opacity: 0, scale: 0.8 }}
@@ -71,9 +75,19 @@ function NavBar() {
           onToggle={handleToggle}
           label="Show menu"
           easing="ease-in"
+          toggled={isOpen}
           size={20}
         />
-        {isOpen}
+        <AnimatePresence>
+          {isOpen && (
+            <MobileMenu
+              isOpen={isOpen}
+              routes={routes}
+              currentRoute={currentRoute}
+              handleToggle={handleToggle}
+            />
+          )}
+        </AnimatePresence>
       </motion.div>
     </nav>
   );
