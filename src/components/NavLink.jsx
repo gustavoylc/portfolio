acefbfa29cd746f40a339e18c1686203d76538ce
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-03-17 11:20:22 -0400

    style: update menu hover

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -2,33 +2,29 @@ import Link from 'next/link';
 
 function NavLink({ href, text, isActive, isMobileMenu = false }) {
   return (
-    <div
-      className={`relative inline-block w-full p-1  ${
-        isActive &&
-        isMobileMenu &&
-        'bg-[#1f2736] rounded hover:bg-[#1f2736] hover:rounded'
-      }
+    <Link href={href}>
+      <div
+        className={`relative inline-block w-full p-1 ease-in duration-150 cursor-pointer  ${
+          isActive &&
+          isMobileMenu &&
+          'bg-[#1f2736] rounded hover:bg-[#1f2736] hover:rounded'
+        }
+      ${isActive ? 'text-blue-500 ' : 'hover:text-white'}
       ${isMobileMenu && 'hover:bg-[#1f2736] hover:rounded'}
       `}
-    >
-      <Link
-        href={href}
-        className={`ease-in duration-150 cursor-pointer ${
-          isActive ? 'text-blue-500 ' : 'hover:text-white'
-        }`}
       >
         {text}
-      </Link>
-      {!isMobileMenu && (
-        <div
-          className={`overflow-hidden h-0.5 relative bg-blue-500 ${
-            isActive ? 'w-full' : 'w-0'
-          } transition-all duration-1000`}
-        >
-          <div className="absolute inset-0 transform origin-center scale-x-0 group-hover:scale-x-100 ease-out duration-200" />
-        </div>
-      )}
-    </div>
+        {!isMobileMenu && (
+          <div
+            className={`overflow-hidden h-0.5 relative bg-blue-500 ${
+              isActive ? 'w-full' : 'w-0'
+            } transition-all duration-1000`}
+          >
+            <div className="absolute inset-0 transform origin-center scale-x-0 group-hover:scale-x-100 ease-out duration-200" />
+          </div>
+        )}
+      </div>
+    </Link>
   );
 }
 
